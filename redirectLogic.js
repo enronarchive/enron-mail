c72829ef500a04fcaf38e5151ba2a7f577f2352c
Facundo Pignanelli <71728057+facundopignanelli@users.noreply.github.com>
2024-02-25 14:10:28 -0300

    0.5

--- redirectLogic.js
+++ redirectLogic.js
@@ -13,7 +13,19 @@ function redirectUser(input) {
         
       case "quenet-j":
         redirectUrl = "/mail/quenet-j";
-        break;  
+        break;
+        
+        case "slinger-r":
+        redirectUrl = "/mail/slinger-r/";
+        break;
+
+        case "rapp-b":
+        redirectUrl = "/mail/rapp-b/";
+        break;
+
+        case "harris-s":
+        redirectUrl = "/mail/harris-s/";
+        break;
       // Add more cases for other pages as needed
       default:
         redirectUrl = "index.html";
